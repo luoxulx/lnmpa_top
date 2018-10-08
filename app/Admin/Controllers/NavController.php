ecc56c9b65c5a2cd4fb83271549350f929f13c89
luoxu <>
2018-10-08 21:31:17 +0800

    xx nav

---
+++ app/Admin/Controllers/NavController.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gunma-php
+ * Date: 2018/10/8
+ * Time: 下午4:24
+ */
+
+namespace App\Admin\Controllers;
+
+
+class NavController extends BaseAdminController
+{
+
+
+}
